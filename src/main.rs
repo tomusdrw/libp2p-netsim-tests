84d99e03641c15709587bba0b211338eb3f5f68c
Tomasz Drwięga <tomusdrw@gmail.com>
2018-09-13 16:43:54 +0200

    Rewrite and use network simulator.

--- src/main.rs
+++ src/main.rs
@@ -2,65 +2,150 @@ extern crate netsim;
 extern crate tokio_core;
 extern crate libp2p;
 
-use std::io::{Read, Write};
-use std::net::UdpSocket;
+use std::io::{Write};
+use std::net::Ipv4Addr;
 use tokio_core::reactor::Core;
-use netsim::{spawn, node, Network, Ipv4Range};
 use libp2p::{tokio_io, Transport};
-use libp2p::futures::{Future, IntoFuture};
+use libp2p::futures::{Future};
 use libp2p::multiaddr::ToMultiaddr;
 
-fn main() {
-    // Create an event loop and a network to bind devices to.
-    let mut core = Core::new().unwrap();
-    let network = Network::new(&core.handle());
+mod network;
 
-    let (tx, rx) = std::sync::mpsc::channel();
+#[derive(Debug)]
+enum NodeKind {
+    Sender,
+    Receiver,
+}
 
-    let receiver_node = node::ipv4::machine(move |ipv4_addr| {
-        let mut core = Core::new().unwrap();
+enum Node {
+    Sender(Sender),
+    Receiver(Receiver),
+}
+
+impl network::RunningNode for Node {
+    type Result = ();
+
+    fn connect_to(&mut self, addr: Ipv4Addr) {
+        match *self {
+            Node::Sender(ref mut sender) => sender.connect_to(addr),
+            Node::Receiver(ref mut recv) => recv.connect_to(addr),
+        }
+    }
+
+    fn wait(self) -> Self::Result {
+        match self {
+            Node::Sender(sender) => sender.wait(),
+            Node::Receiver(recv) => recv.wait(),
+        }
+    }
+}
+
+struct Receiver {
+    core: Core,
+    controller: libp2p::core::SwarmController<libp2p::core::transport::dummy::DummyMuxing<libp2p::CommonTransport>>,
+    run: Option<Box<Future<Item = (), Error = ()>>>,
+}
+
+impl Receiver {
+    pub fn new(id: network::NodeId, addr: Ipv4Addr) -> Self {
+        let core = Core::new().unwrap();
         let transport = libp2p::CommonTransport::new();
-
+        let mut multiaddr = addr.to_multiaddr().unwrap();
+        multiaddr.append(libp2p::multiaddr::AddrComponent::TCP(1025));
         let (controller, run) = libp2p::swarm(
             transport.with_dummy_muxing(),
-            |future, _| {
+            move |future, _| {
+                let id = id.clone();
                 tokio_io::io::read_to_end(future, vec![])
-                    .map(|(_more, res)| {
-                        println!("[node1] Received: {:?}", String::from_utf8(res));
+                    .map(move |(_more, res)| {
+                        println!("[{}] Received: {:?}", id, String::from_utf8(res));
                     })
             },
         );
 
-        let mut multiaddr = ipv4_addr.to_multiaddr().unwrap();
+        let _addr = controller.listen_on(multiaddr.clone()).unwrap();
+
+        Receiver {
+            core,
+            controller,
+            run: Some(Box::new(run.map_err(|e| panic!("{:?}", e)))),
+        }
+    }
+}
+
+impl network::RunningNode for Receiver {
+    type Result = ();
+
+    fn connect_to(&mut self, addr: Ipv4Addr) {
+        let mut multiaddr = addr.to_multiaddr().unwrap();
         multiaddr.append(libp2p::multiaddr::AddrComponent::TCP(1025));
-        let addr = controller.listen_on(multiaddr.clone()).unwrap();
-        println!("[node1] Listening on {:?}", addr);
-        tx.send(multiaddr).unwrap();
+        self.controller.dial(multiaddr, libp2p::CommonTransport::new()).unwrap();
+    }
 
-        core.run(run)
+    fn wait(mut self) -> Self::Result {
+        let _res = self.core.run(self.run.take().unwrap());
+
+        ()
+    }
+}
+
+struct Sender {
+    core: Core,
+    transport: Option<libp2p::CommonTransport>,
+    run: Option<Box<Future<Item = (), Error = ()>>>,
+}
+
+impl Sender {
+    pub fn new(_id: network::NodeId) -> Self {
+        Sender {
+            core: Core::new().unwrap(),
+            transport: Some(libp2p::CommonTransport::new()),
+            run: None,
+        }
+    }
+}
+
+impl network::RunningNode for Sender {
+    type Result = ();
+
+    fn connect_to(&mut self, addr: Ipv4Addr) {
+        let mut multiaddr = addr.to_multiaddr().unwrap();
+        multiaddr.append(libp2p::multiaddr::AddrComponent::TCP(1025));
+        let transport = self.transport.take().unwrap();
+        self.run = Some(Box::new(transport.dial(multiaddr)
+            .unwrap()
+            .map(|(mut stream, _addr)| {
+                println!("[node2] Connected.");
+                stream.write_all(b"Hello World!").unwrap();
+                println!("[node2] Wrote data.");
+            })
+            .map_err(|e| panic!("{:?}", e))
+        ));
+    }
+
+    fn wait(mut self) -> Self::Result {
+        let _res = self.core.run(self.run.take().unwrap());
+
+        ()
+    }
+}
+
+
+fn main() {
+    let mut network = network::Network::default();
+    let a = network.node("node1", NodeKind::Sender);
+    let b = network.node("node2", NodeKind::Receiver);
+
+    network.connect_all(&[b, a]);
+
+    network.start(|id, kind, addr| {
+    match kind {
+            NodeKind::Receiver => {
+                Node::Sender(Sender::new(id))
+            },
+            NodeKind::Sender => {
+                Node::Receiver(Receiver::new(id, addr))
+            },
+        }
     });
-
-    // Create the machine which will send the UDP packet
-    let sender_node = node::ipv4::machine(move |_ipv4_addr| {
-        let mut core = Core::new().unwrap();
-        let receiver_multiaddr = rx.recv().unwrap();
-        let transport = libp2p::CommonTransport::new();
-        let connection = transport.dial(receiver_multiaddr).unwrap();
-        core.run(connection.map(|(mut stream, _addr)| {
-            println!("[node2] Connected.");
-            stream.write_all(b"Hello World!").unwrap();
-            println!("[node2] Wrote data.");
-        }))
-    });
-
-    // Connect the sending and receiving nodes via a router
-    let router_node = node::ipv4::router((receiver_node, sender_node));
-
-    // Run the network with the router as the top-most node. `_plug` could be used send/receive
-    // packets from/to outside the network
-    let (spawn_complete, _plug) = spawn::ipv4_tree(&network.handle(), Ipv4Range::global(), router_node);
-
-    // Drive the network on the event loop and get the data returned by the receiving node.
-    let _result = core.run(spawn_complete).unwrap();
-    // assert_eq!(&received[..], b"hello world");
 }
---
+++ src/network.rs
@@ -0,0 +1,147 @@
+use std::{
+    collections::HashMap,
+    fmt,
+    net::Ipv4Addr,
+    sync::mpsc,
+};
+use tokio_core::reactor::Core;
+use netsim::{self, spawn, node, Ipv4Range};
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct NodeId(String);
+
+impl fmt::Display for NodeId {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        write!(fmt, "{}", self.0)
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Node<NodeKind> {
+    id: NodeId,
+    kind: NodeKind,
+}
+
+#[derive(Debug)]
+pub struct Network<NodeKind> {
+    nodes:  HashMap<NodeId, Node<NodeKind>>,
+    connections: Vec<
+        (NodeId, NodeId),
+    >
+}
+
+impl<NodeKind> Default for Network<NodeKind> {
+    fn default() -> Self {
+        Network {
+            nodes: Default::default(),
+            connections: Default::default(),
+        }
+    }
+}
+
+impl<NodeKind> Network<NodeKind> where
+    NodeKind: Send + 'static,
+{
+    pub fn node<T: Into<String>>(&mut self, id: T, kind: NodeKind) -> NodeId {
+        let id = NodeId(id.into());
+        let node = Node {
+            id: id.clone(),
+            kind,
+        };
+        let prev = self.nodes.insert(id.clone(), node);
+        assert!(prev.is_none(), "Duplicate id: {:?}", id);
+
+        id
+    }
+
+    pub fn connect_all(&mut self, nodes: &[NodeId]) {
+        for (idx, a) in nodes.iter().enumerate() {
+            for b in nodes.iter().skip(idx + 1) {
+                self.connections.push((a.clone(), b.clone()));
+            }
+        }
+    }
+
+    pub fn start<R, F, N>(self, node_runner: F) -> HashMap<NodeId, R>
+    where
+        F: Fn(NodeId, NodeKind, Ipv4Addr) -> N + Send + Clone + 'static,
+        N: RunningNode<Result=R>,
+        R: Send + 'static
+    {
+        let mut core = Core::new().unwrap();
+        let network = netsim::Network::new(&core.handle());
+
+
+        let mut nodes = vec![];
+        let mut addresses = HashMap::new();
+        let mut connect_to = HashMap::new();
+
+        for (id, node) in self.nodes {
+            let (addr_tx, addr_rx) = mpsc::channel();
+            let (conn_tx, conn_rx) = mpsc::channel();
+            addresses.insert(id.clone(), addr_rx);
+            connect_to.insert(id.clone(), conn_tx);
+            let node_runner = node_runner.clone();
+
+            nodes.push(node::ipv4::machine(move |addr| {
+                println!("[{:?}] Starting", id);
+                addr_tx.send(addr).expect("Network not running");
+                let mut node = node_runner(node.id, node.kind, addr);
+                println!("[{:?}] Waiting for connections", id);
+                for addr in conn_rx {
+                    node.connect_to(addr);
+                }
+                println!("[{:?}] Running", id);
+                let res = (id.clone(), node.wait());
+                println!("[{:?}] Done", id);
+                res
+            }));
+        }
+
+        // Connect the sending and receiving nodes via a router
+        let router_node = node::ipv4::router(nodes);
+
+        // Run the network with the router as the top-most node. `_plug` could be used send/receive
+        // packets from/to outside the network
+        let (spawn_complete, _plug) = spawn::ipv4_tree(&network.handle(), Ipv4Range::global(), router_node);
+
+
+        // Make sure we collect the addresses an connect to each other.
+        let mut addr = HashMap::new();
+        for (id, addr_rx) in addresses {
+            match addr_rx.recv() {
+                Ok(a) => {
+                    addr.insert(id, a);
+                },
+                Err(e) => {
+                    println!("Unable to get address of {:?}: {:?}", id, e);
+                },
+            }
+        }
+        // connect to each other
+        for (a, b) in self.connections {
+            match (connect_to.get(&a), addr.get(&b)) {
+                (Some(tx), Some(addr)) => {
+                    tx.send(addr.clone()).expect("Node should be listening for connections.");
+                },
+                _ => {},
+            }
+        }
+        // make sure to drop tx ends of connect_to channels.
+        drop(connect_to);
+
+        // Drive the network on the event loop and get the data returned by the receiving node.
+        let res = core.run(spawn_complete).unwrap();
+        res.into_iter().collect()
+    }
+}
+
+pub trait RunningNode {
+    type Result;
+
+    /// Attempt to connect to a particular address.
+    fn connect_to(&mut self, addr: Ipv4Addr);
+
+    /// Wait for this node to run to completion and return a result.
+    fn wait(self) -> Self::Result;
+}
